a5313cc222ad7b179627eef1a64512346ae3c4d0
DeividZavala <crusedmoss@gmail.com>
2020-07-27 20:21:38 -0500

    test description improved

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,13 @@ import { render, screen, waitFor } from "@testing-library/react";
 import { server, rest } from "./mocks/server";
 import App from "./App";
 
-test("render todos", async () => {
+test("fetch todos and render", async () => {
   render(<App />);
   const items = await waitFor(() => screen.getAllByRole("listitem"));
   expect(items.length).toBe(1);
 });
 
-test("render todos", async () => {
+test("fetch todos and render failed", async () => {
   server.use(
     rest.get("http://localhost:4000/todos", (req, res, ctx) => {
       return res(ctx.status(500), ctx.json({ message: "http error" }));
